c92136a866ab80285b657e84e5ff302346b317c0
Nicholas Porter <nicholas@metax.io>
2018-03-06 10:21:28 -0700

    Attempting to implement interface for expense functionality

--- main.go
+++ main.go
@@ -8,28 +8,23 @@ import(
 	"strings"
 )
 
-func main(){
-
-	type DailyExpenses struct{
-		lunch int
-		dinner int
-	}
-
-	getMonthlyExpenses()
-	getWeeklyExpenses()
-
+type MonthlyExpenses struct{
+	rent int
+	carInsurance int
+	rentersInsurance int
+	gifts int
+	travel int
+	healthInsurance int
 }
 
-func getMonthlyExpenses(){
+//type Expenses interface{
+//	loadExpensePrompts()
+//	getMonthlyExpenses()
+//	getWeeklyExpenses()
+//	getDailyExpenses()
+//}
 
-	type monthlyExpenses struct{
-		rent int
-		carInsurance int
-		rentersInsurance int
-		gifts int
-		travel int
-		healthInsurance int
-	}
+func getMonthlyExpenses() []int {
 
 	reader := bufio.NewReader(os.Stdin)
 
@@ -75,23 +70,20 @@ func getMonthlyExpenses(){
 
 	// Assign data back to pointed reference of expense struct
 
-	s := monthlyExpenses{rent, carInsurance, rentersInsurance, gifts, travel, healthInsurance}
-	sp := &s
-	sp.rent = rent
+	s []int = [rent, carInsurance, rentersInsurance, gifts, travel, healthInsurance]
 	fmt.Println(s)
+	return s
 
 }
 
 
+type weeklyExpenses struct{
+	gas int
+	drinks int
+	groceries int
+}
 
-func getWeeklyExpenses(){
-
-	type weeklyExpenses struct{
-		gas int
-		drinks int
-		groceries int
-	}
-
+func getWeeklyExpenses() weeklyExpenses{
 	reader := bufio.NewReader(os.Stdin)
 
 	fmt.Println("-----Weekly Expenses----")
@@ -117,6 +109,60 @@ func getWeeklyExpenses(){
 	s := weeklyExpenses{gas, drinks, groceries}
 // 	sp := &s
 	fmt.Println(s)
+	return s
+
+}
+
+type dailyExpenses struct{
+	lunch int
+	dinner int
+}
+
+func getDailyExpenses() dailyExpenses{
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("-----Daily Expenses----")
+	fmt.Println("")
+
+	fmt.Println("Enter Lunch Expense: ")
+	input1, _  := reader.ReadString('\n')
+	format1 := strings.Split(input1, "\n")[0]
+	lunch, _ := strconv.Atoi(format1)
+
+	fmt.Println("Enter Dinner Expense: ")
+	input2, _  := reader.ReadString('\n')
+	format2 := strings.Split(input2, "\n")[0]
+	dinner, _ := strconv.Atoi(format2)
+
+	// Assign data back to pointed reference of expense struct
+
+	s := dailyExpenses{lunch, dinner}
+	fmt.Println(s)
+	return s
+
+}
+
+func calculateAverage(){
+// Monthly * 12
+	monthly := getMonthlyExpenses()
+	j := 0
+	for i := 0; i < 6 ; i++ {
+		j += monthly[i]
+
+	}
+	fmt.Println(j)
+// Weekly * 52
+// Daily * 365
+
+// Total / 365
+}
+
+func main(){
+	calculateAverage()
+	//	getMonthlyExpenses()
+	getWeeklyExpenses()
+	getDailyExpenses()
 
 }
 
